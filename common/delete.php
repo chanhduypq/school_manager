8a401fe5581872e9e000351f97fc4861964513a6
chanhduypq <chanhduypq@gmail.com>
2017-10-18 02:44:13 +0700

    sau khi xóa pupil, phải xóa luôn file avatar và profile trên server nếu có

--- common/delete.php
+++ common/delete.php
@@ -1,11 +1,28 @@
-<?php 
+<?php
+
 $conn = mysqli_connect("localhost", "root", "", "school_manager") or die();
 mysqli_query($conn, "set names 'utf8'");
 $id = $_GET['id'];
 $tableName = $_GET['table_name'];
-if (!ctype_digit($id)) {    
+if (!ctype_digit($id)) {
     exit;
 }
-$sql="delete from $tableName where id=$id";
+
+
+if ($tableName == 'pupil') {
+    deleteAvatarProfile($conn, $id);
+}
+
+$sql = "delete from $tableName where id=$id";
 mysqli_query($conn, $sql);
+
+function deleteAvatarProfile($conn, $id) {
+    $sql = "select * from pupil where id=$id";
+    $result = mysqli_query($conn, $sql);
+    while ($row = mysqli_fetch_array($result)) {
+        @unlink("../public/images/database/avatar/" . $row['avatar']);
+        @unlink("../public/images/database/profile/" . $row['profile']);
+    }
+}
+
 ?>
