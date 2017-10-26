a8fe862976cb3d8b4cd81c425b881a0646b49062
chanhduypq <chanhduypq@gmail.com>
2017-10-26 23:53:47 +0700

    phân trang hoàn chỉnh cho pupil

--- pupil/index.php
+++ pupil/index.php
@@ -4,6 +4,13 @@ if (!isset($_SESSION['username'])) {
     header('Location:../login.php');
     exit;
 }
+include '../define.php';
+if (isset($_GET['page']) && ctype_digit($_GET['page'])) {
+    $page = $_GET['page'];
+} else {
+    $page = 1;
+}
+$offset = ($page - 1) * NUMBER_ROW_PERPAGE;
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,7 +18,8 @@ if (!isset($_SESSION['username'])) {
         <title>Học sinh</title>
         <meta http-equiv="content-type" content="text/html;charset=utf-8;" />
         <link href="../public/css/style.css" rel="stylesheet" type="text/css"/>
-        <link href="../public/css/menu.css" rel="stylesheet" type="text/css"/>   
+        <link href="../public/css/menu.css" rel="stylesheet" type="text/css"/> 
+        <link href="../public/css/paging.css" rel="stylesheet" type="text/css"/>
         <script src="../public/js/jquery-2.0.3.js"></script>
         <script type="text/javascript">
             jQuery(function($){
@@ -58,10 +66,10 @@ if (!isset($_SESSION['username'])) {
                 <th style="width: 20%;">&nbsp;</th>
             </tr>
             <?php 
-            include '../define.php';
             $conn = mysqli_connect(HOST, USERNAME, PASSWORD, DB_NAME) or die();
             mysqli_query($conn, "set names 'utf8'");
-            $result = mysqli_query($conn, "select * from pupil_full order by class_id ASC");
+            $countPupil = getClassPupil($conn);
+            $result = mysqli_query($conn, "select * from pupil_full order by class_id ASC limit $offset," . NUMBER_ROW_PERPAGE);
             while ($row = mysqli_fetch_array($result)) {
                 ?>
                 <tr>
@@ -122,6 +130,74 @@ if (!isset($_SESSION['username'])) {
                 </tr>
                 <?php
             }
+            if ($countPupil > 0) {
+                $numberPage = ceil($countPupil / NUMBER_ROW_PERPAGE);
+
+                $rangeCount = 3;
+
+                $rangeNumber = ceil($numberPage / $rangeCount);
+                $range = ceil($page / $rangeCount);
+                $start = $range * $rangeCount - ($rangeCount - 1);
+
+                if ($page == 1) {
+                    $hrefPrev = "#";
+                    $hrefFirst = "#";
+                    $classPrevFirst = "selected";
+                } else {
+                    $hrefFirst = "index.php?page=1";
+                    $hrefPrev = "index.php?page=" . ($page - 1);
+                    $classPrevFirst = "not_selected";
+                }
+
+                if ($page == $numberPage) {
+                    $hrefNext = "#";
+                    $hrefLast = "#";
+                    $classNextLast = "selected";
+                } else {
+                    $hrefLast = "index.php?page=$numberPage";
+                    $hrefNext = "index.php?page=" . ($page + 1);
+                    $classNextLast = "not_selected";
+                }
+                ?>
+                <tr style="height: 50px;background-color: #c1976c;">
+                    <td colspan="8" style="text-align: center;" class="pagination">
+
+                        <span class="<?php echo $classPrevFirst; ?>" onclick="window.location = '<?php echo $hrefFirst; ?>';">
+                            First
+                        </span>
+                        <span class="<?php echo $classPrevFirst; ?>" onclick="window.location = '<?php echo $hrefPrev; ?>';">
+                            Previous
+                        </span>
+
+                        <?php
+                        for ($i = 1; $i <= 3 && $start <= $numberPage; $i++) {
+                            if ($page == $start) {
+                                $href = "#";
+                                $class = 'selected';
+                            } else {
+                                $href = "index.php?page=$start";
+                                $class = 'not_selected';
+                            }
+                            ?>
+                            <span class="<?php echo $class; ?>" onclick="window.location = '<?php echo $href; ?>';">
+                                <?php echo $start; ?>
+                            </span>
+                            <?php
+                            $start++;
+                        }
+                        ?>
+
+                        <span class="<?php echo $classNextLast; ?>" onclick="window.location = '<?php echo $hrefNext; ?>';">
+                            Next
+                        </span>
+                        <span class="<?php echo $classNextLast; ?>" onclick="window.location = '<?php echo $hrefLast; ?>';">
+                            Last
+                        </span>
+
+                    </td>
+                </tr>
+                <?php
+            }
             ?>
         </table>
     </body>
@@ -135,5 +211,13 @@ function convertToVNDate($dateTime) {
     $temp = explode('-', $dateEn);
     $dateVn = $temp[2] . '/' . $temp[1] . '/' . $temp[0];
     return $dateVn;
+}
+
+function getClassPupil($conn) {
+    $result = mysqli_query($conn, "SELECT count(*) as count FROM pupil");
+    if ($row = mysqli_fetch_array($result)) {
+        return $row['count'];
+    }
+    return 0;
 }
 ?>
